d6caad991f4a0e5270b474da9dc39a47d9e0403f
Arthur Martins Ribeiro <ribeiro.arthur25@gmail.com>
2025-01-09 23:25:13 -0300

    adding context provider to the aplication

--- quiz/src/components/Welcome.jsx
+++ quiz/src/components/Welcome.jsx
@@ -1,12 +1,20 @@
+import { useContext } from 'react'
+import { QuizContext } from '../context/quiz'
+
 import Quiz from '../img/quiz.svg'
 import './Welcome.css'
 
 
 const Welcome = () => {
+
+  const quizState = useContext(QuizContext)
+  
+  console.log(quizState)
+
   return (
     <div id="welcome">
       <h2>Welcome!</h2>
-        <p>click in the button below to get started:</p>
+        <p>Click in the button below to get started:</p>
       <button>Start</button>
       <img src={Quiz} alt="quiz-beginning" />
     </div>
