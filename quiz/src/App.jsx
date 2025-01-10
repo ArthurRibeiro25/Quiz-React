18342ea64e87ad22d6fdbe898383c79f221a698e
Arthur Martins Ribeiro <ribeiro.arthur25@gmail.com>
2025-01-10 00:40:13 -0300

    button change state and redirects to question page

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,13 +1,19 @@
-import Welcome from './components/Welcome';
+import { useContext } from 'react';
+import { QuizContext } from './context/quiz';
+
+import Welcome from './components/Welcome'
+import Question from './components/Question.jsx'
 
 import './App.css'
 
 function App() {
-  
+  const [quizState, dispatch] = useContext(QuizContext)
+
   return (
     <div className="App">
       <h1>Dev Quiz</h1>
-      <Welcome />
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
     </div>
   );
 }
